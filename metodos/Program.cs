b119ed018e1fb5b0f958e8be330b77de073941ee
KEL <kellogical1@gmail.com>
2024-10-14 01:03:12 -0300

    acrescetando

---
+++ metodos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+
+namespace metodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Cria uma instância da classe MinhaClasse
+            MinhaClasse minhaClasse = new MinhaClasse();
+
+          
+
+
+            // Chama o método Saudacao
+            minhaClasse.Saudacao();
+        }
+    }
+
+    // Define a classe MinhaClasse
+    public class MinhaClasse
+    {
+        public void Saudacao()
+        {
+            Console.WriteLine("Bem vindo");
+        }
+    }
+}
